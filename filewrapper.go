8af6265bf9b5349d8ab3d6704a99f5221e100075
hc <chrislue1986@hotmail.com>
2016-12-30 19:30:11 +0800

    upadte

--- filewrapper.go
+++ filewrapper.go
@@ -2,13 +2,57 @@ package goblazer
 
 import (
 	"os"
+	"path/filepath"
+	"strings"
 )
 
-// IsFileExisted  a function wrapper to check file existed or not
+var szAppRootPath string
+
+func init() {
+	SetAppRootPath("")
+}
+
+// IsFileExisted : a function wrapper to check file existed or not
 func IsFileExisted(szFilePath string) bool {
 	_, err := os.Stat(szFilePath)
 	if os.IsNotExist(err) {
 		return false
 	}
 	return true
+}
+
+// FormatFilePath : a function wrapper to format path to "a/b/c/d"
+func FormatFilePath(path *string) {
+	*path = strings.Replace(*path, "\\", "/", -1)
+	*path = strings.Trim(*path, "/ ")
+}
+
+// SetAppRootPath : a function wrapper to set current application base path, if parameter 'path' is nil or "",
+// 					it will fetch base path by current executable program.
+func SetAppRootPath(path string) bool {
+	var err error
+
+	if path != "" {
+		if FormatFilePath(&path); IsFileExisted(path) {
+			szAppRootPath = path
+			return true
+		}
+	}
+
+	if path, err = filepath.Abs(filepath.Dir(os.Args[0])); err != nil {
+		szAppRootPath = ""
+		return false
+	}
+
+	if FormatFilePath(&path); IsFileExisted(path) {
+		szAppRootPath = path
+		return true
+	}
+
+	return false
+}
+
+// GetAppRootPath : a function wrapper to get current application path
+func GetAppRootPath() string {
+	return szAppRootPath
 }
